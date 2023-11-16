04c6ca13a44c6108353b3d92020e2e540f2c76ee
copper <79734708+OlympianGames@users.noreply.github.com>
2023-11-16 14:10:23 -0600

    imgui labs testing

--- CopperEngine.Labs/ImGuiTesting.cs
+++ CopperEngine.Labs/ImGuiTesting.cs
@@ -1,17 +1,7 @@
-﻿/*******************************************************************************************
- *
- *   raylib-extras [ImGui] example - Simple Integration
- *
- *	This is a simple ImGui Integration
- *	It is done using C++ but with C style code
- *	It can be done in C as well if you use the C ImGui wrapper
- *	https://github.com/cimgui/cimgui
- *
- *   Copyright (c) 2021 Jeffery Myers
- *
- ********************************************************************************************/
-
+﻿using System.Numerics;
+using CopperEngine.Data;
 using CopperEngine.Editor.DearImGui;
+using CopperEngine.Utility;
 using ImGuiNET;
 using Raylib_CsLo;
 
@@ -19,22 +9,87 @@ namespace CopperEngine.Labs;
 
 public class ImGuiTesting
 {
+
+    private static RenderTexture gameTexture;
+    private static Camera gameCamera = new()
+    {
+        Position = new Vector3(10, 10, 10),
+        Target = Vector3.Zero
+    };
+    
+    private static RenderTexture editorTexture;
+    private static Camera editorCamera = new()
+    {
+        Position = new Vector3(-10, 10, -10),
+        Target = Vector3.Zero
+    };  
     
     public static void Run()
     {
         Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_VSYNC_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
-        Raylib.InitWindow(650, 400, "raylib-Extras-cs [ImGui] example - simple ImGui Demo");
+        Raylib.InitWindow(650, 400, "imgui render texture testing");
         Raylib.SetTargetFPS(144);
+        
+        gameTexture = Raylib.LoadRenderTexture(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+        editorTexture = Raylib.LoadRenderTexture(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
 
-        rlImGui.Setup(true);
+        rlImGui.Setup();
 
         while (!Raylib.WindowShouldClose())
         {
+            if (Raylib.IsWindowResized())
+            {
+                Raylib.UnloadRenderTexture(gameTexture);
+                gameTexture = Raylib.LoadRenderTexture(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+                Raylib.UnloadRenderTexture(editorTexture);
+                editorTexture = Raylib.LoadRenderTexture(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+            }
+            
             Raylib.BeginDrawing();
+            
+            // game
+            Raylib.BeginTextureMode(gameTexture);
+            Raylib.ClearBackground(ColorUtil.DarkGray);
+            Raylib.BeginMode3D(gameCamera);
+            RenderScene();
+            Raylib.EndMode3D();
+            Raylib.EndTextureMode();
+            
+            // editor
+            Raylib.BeginTextureMode(editorTexture);
+            Raylib.ClearBackground(ColorUtil.DarkGray);
+            Raylib.BeginMode3D(editorCamera);
+            RenderScene();
+            Raylib.EndMode3D();
+            Raylib.EndTextureMode();
+            
+            // ui
             Raylib.ClearBackground(Raylib.DARKGRAY);
-
+            
             rlImGui.Begin();
             ImGui.ShowDemoWindow();
+            
+            
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(0, 0));
+
+            if (ImGui.Begin("Game"))
+            {
+                rlImGui.ImageRenderTextureFit(gameTexture);
+                ImGui.End();
+            }
+            
+            ImGui.PopStyleVar();
+            
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(0, 0));
+
+            if (ImGui.Begin("Editor"))
+            {
+                rlImGui.ImageRenderTextureFit(editorTexture);
+                ImGui.End();
+            }
+            
+            ImGui.PopStyleVar();
+            
             rlImGui.End();
 
             Raylib.EndDrawing();
@@ -42,5 +97,10 @@ public class ImGuiTesting
 
         rlImGui.Shutdown();
         Raylib.CloseWindow();
+    }
+
+    private static void RenderScene()
+    {
+        Raylib.DrawCube(Vector3.Zero, 1, 1, 1, ColorUtil.Red);
     }
 }
--- CopperEngine/Data/Camera.cs
+++ CopperEngine/Data/Camera.cs
@@ -8,16 +8,35 @@ namespace CopperEngine.Data;
 
 public class Camera
 {
-    public RaylibCamera Camera3D = new(Vector3.Zero, Vector3.One, Vector3.UnitY, 45, CameraProjection.CAMERA_PERSPECTIVE);
+    private RaylibCamera Camera3D = new(Vector3.Zero, Vector3.One, Vector3.UnitY, 45, CameraProjection.CAMERA_PERSPECTIVE);
 
-    public Vector3 Position => Camera3D.position;
+    public Vector3 Position
+    {
+        get => Camera3D.position;
+        set => Camera3D.position = value;
+    }
+
+    public Vector3 Target
+    {
+        get => Camera3D.target;
+        set => Camera3D.target = value;
+    }
+
     // public Vector3 Front = new(0.0f, 0.0f, -1.0f);
     public Vector3 Up => Camera3D.up;
     public Vector3 Direction => Vector3.Normalize(Camera3D.target - Position);
+
     // public float Yaw = -90f;
     // public float Pitch = 0f;
-    public float Zoom => Camera3D.fovy;
+    public float Zoom
+    {
+        get => Camera3D.fovy;
+        set => Camera3D.fovy = value;
+    }
+
     public Vector2 ClippingPlane = new(0.1f, 100f);
+
+    public static implicit operator RaylibCamera(Camera camera) => camera.Camera3D;
     
     public Matrix4x4 ViewMatrix => Matrix4x4.CreateLookAt(Position, Camera3D.target, Up);
     
