db9ffdbf508404b7c50eed657408302e535d909f
copper <79734708+OlympianGames@users.noreply.github.com>
2023-12-04 17:46:03 -0600

    random list and other stuff

---
+++ CopperEngine.Labs/UtilLab.cs
@@ -0,0 +1,26 @@
+﻿using CopperEngine.Logs;
+using CopperEngine.Utility;
+
+namespace CopperEngine.Labs;
+
+public class UtilLab
+{
+    public static void Run()
+    {
+        var randomFloatList = new RandomList<float>(new List<float> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        var randomIntList = new RandomList<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) { 5, 5, 6 };
+        var randomByteList = new RandomList<byte> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        
+        for (var i = 0; i < 5; i++)
+        {
+            float randomFloatValue = randomFloatList;
+            Log.Info(randomFloatValue);
+            
+            float randomIntValue = randomIntList;
+            Log.Info(randomIntValue);
+            
+            float randomByteValue = randomByteList;
+            Log.Info(randomByteValue);
+        }
+    }
+}
---
+++ CopperEngine/Utility/RandomList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace CopperEngine.Utility;
+
+public class RandomList<T> : IEnumerable<T>
+{
+    private readonly Random listRandom = new(Guid.NewGuid().GetHashCode());
+    private readonly List<T> list;
+
+    public RandomList(IEnumerable<T> list)
+    {
+        this.list = list.ToList();
+    }
+    
+    public RandomList(params T[] list)
+    {
+        this.list = list.ToList();
+    }
+
+    public RandomList()
+    {
+        this.list = new List<T>();
+    }
+
+    public static implicit operator T(RandomList<T> value)
+    {
+        return value.list[value.listRandom.Next(value.list.Count-1)];
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return list.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T value)
+    {
+        list.Add(value);
+    }
+}
